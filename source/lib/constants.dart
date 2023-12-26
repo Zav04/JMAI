1a4d035082c5d4d383e85b2ed4773c289e8ef7e9
Bruno Oliveira <bruno.bx04@gmail.com>
2023-12-26 14:43:32 +0000

    Add Flutter SW

---
+++ source/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.blue;
+const secondaryColor = Color(0xFFf4f5fd);
+const bgColor = Colors.white;
+
+const defaultPadding = 16.0;
---
+++ source/lib/screens/main/components/side_menu.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:admin/constants.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: secondaryColor,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: SvgPicture.asset("assets/images/logo-color.svg"),
+            ),
+            DrawerListTile(
+              title: "Dashboard",
+              svgSrc: "assets/icons/menu_dashboard.svg",
+              press: () {},
+            ),
+            DrawerListTile(
+              title: "Verificação de Requerimentos",
+              svgSrc: "assets/icons/menu_tran.svg",
+              press: () {},
+            ),
+            DrawerListTile(
+              title: "Task",
+              svgSrc: "assets/icons/menu_task.svg",
+              press: () {},
+            ),
+            DrawerListTile(
+              title: "Documents",
+              svgSrc: "assets/icons/menu_doc.svg",
+              press: () {},
+            ),
+            DrawerListTile(
+              title: "Store",
+              svgSrc: "assets/icons/menu_store.svg",
+              press: () {},
+            ),
+            DrawerListTile(
+              title: "Notification",
+              svgSrc: "assets/icons/menu_notification.svg",
+              press: () {},
+            ),
+            DrawerListTile(
+              title: "Profile",
+              svgSrc: "assets/icons/menu_profile.svg",
+              press: () {},
+            ),
+            DrawerListTile(
+              title: "Settings",
+              svgSrc: "assets/icons/menu_setting.svg",
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerListTile extends StatefulWidget {
+  const DrawerListTile({
+    Key? key,
+    required this.title,
+    required this.svgSrc,
+    required this.press,
+  }) : super(key: key);
+
+  final String title, svgSrc;
+  final VoidCallback press;
+
+  @override
+  _DrawerListTileState createState() => _DrawerListTileState();
+}
+
+class _DrawerListTileState extends State<DrawerListTile> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: Container(
+        color: _isHovered
+            ? Colors.lightBlueAccent
+            : secondaryColor, // Cor do retângulo ao passar o mouse
+        child: ListTile(
+          onTap: widget.press,
+          horizontalTitleGap: 0.0,
+          leading: SvgPicture.asset(
+            widget.svgSrc,
+            color: _isHovered ? Colors.grey[700] : Colors.black,
+            height: 16,
+          ),
+          title: Text(
+            widget.title,
+            style: TextStyle(
+              color: _isHovered ? Colors.white : Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
